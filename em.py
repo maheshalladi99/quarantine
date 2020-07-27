7f27eaff6e523102493533131e28be839b29b901
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2020-07-27 10:49:04 +0530

    Update em.py

--- em.py
+++ em.py
@@ -3,6 +3,8 @@
 ###print(x.translate(non_bmp_map))
 ##print('This works in IDLE too! \U0001F44D'.translate(non_bmp_map))
 ##print('This works outside IDLE! \U0001F44D')
+
+
 for tweet in public_tweets:
     print (tweet.text)
     u=tweet.text
